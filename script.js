399c07d2fbfb2b1e9126fd3096136c17c3d9908c
Carbon4eGG <z198813@gmail.com>
2026-07-29 17:44:38 +1000

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@
   [
     { 
       label: "eXpress", 
-      url: "https://express.ms/info?phone=79997937906", 
+      url: "expressms://chat?phone=79997937906", 
       colorClass: "btn-express",
       iconType: "png",
       iconSrc: "icons/express.png"
